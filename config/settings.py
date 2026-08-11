6bf8b2142db620b6d412dd9588ba28b2eea801c0
eliasfn05-cmd <eliasfn05@gmail.com>
2026-08-11 17:26:42 -0500

    Sprint 7.9.5: load EngineConfig for Premium replacement signals

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "engine",
+    "engine.apps.EngineConfig",
     "scanner",
     "backtesting",
     "dashboard",
