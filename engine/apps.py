b89a323fe80b66b4ed469bcdc5194a6e3a789826
eliasfn05-cmd <eliasfn05@gmail.com>
2026-08-11 17:24:57 -0500

    Sprint 7.9.5: enable Premium replacement status signals

---
+++ engine/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class EngineConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "engine"
+
+    def ready(self):
+        from . import signals  # noqa: F401
