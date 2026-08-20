a74dad44e5994d96d6dcc3afe988ddc051611da7
eliasfn05-cmd <eliasfn05@gmail.com>
2026-08-20 18:32:21 -0500

    fix(btts-v2.1): activate precision policy

--- engine/apps.py
+++ engine/apps.py
@@ -9,6 +9,8 @@ class EngineConfig(AppConfig):
         from . import signals  # noqa: F401
         from .btts_h2h_guard import install_h2h_guard
         from .btts_v2_policy import install_btts_v2_policy
+        from .btts_v21_policy import install_btts_v21_policy
 
         install_h2h_guard()
         install_btts_v2_policy()
+        install_btts_v21_policy()
